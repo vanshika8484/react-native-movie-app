a9c5ad35ecdd609e43b71d45886b4962500e9057
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-01-25 17:09:18 +0530

    create homescreen UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,21 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, Text, View } from "react-native";
+import Searchbutton from "@/components/searchbutton";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router=useRouter();
   return (
-    <View className="flex-1 justify-center items-center" >
-      <Text className="text-2xl font-bold text-dark-200">Edit app/index.tsx to edit this screen.</Text>
-      
-    </View>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="w-full absolute z-0"/>
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom:10, minHeight:"100%"}}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      <View className="flex-1 mt-5">
+        <Searchbutton onPress={()=>router.push("/search")}
+          placeholder="Search for a movie"/>
+\      </View>
+      </ScrollView>
+      </View>
   );
 }
