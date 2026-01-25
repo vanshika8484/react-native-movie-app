609a96ddb4ec343ba94cf0f34ac0ee98573cac67
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-01-25 21:24:33 +0530

    develop search screen

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,76 @@
-import { Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { images } from "@/constants/images";
+import MovieCard from "@/components/MovieCard";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import { icons } from "@/constants/icons";
+import Searchbutton from "@/components/searchbutton";
 function search() {
+const [searchQuery, setSearchQuery] = useState('')
+  const {data:movies,loading,error,refetch:loadMovies,reset}=useFetch(()=>fetchMovies({
+    query:searchQuery
+  }),false)
+  useEffect(()=>{
+    const timeoutId=setTimeout(async()=>{
+if(searchQuery.trim()){
+  await loadMovies();
+}
+else{
+  reset();
+}
+    },500);
+
+  return()=>clearTimeout(timeoutId);
+  },[searchQuery])
   return (
-    <View className="flex-1 justify-center items-center" >
-         <Text className="text-2xl font-bold text-dark-200">Search The Movie</Text>
-         
+    <View className="flex-1 bg-primary" >
+<Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover" />   
+<FlatList data={movies} renderItem={({item})=>(<MovieCard {...item}/>)} keyExtractor={(item)=>item.id.toString()} className="px-5" numColumns={3} columnWrapperStyle={{justifyContent:'center',gap:16,marginVertical:16}} contentContainerStyle={{paddingBottom:100}} 
+
+ListHeaderComponent={
+  <>
+  <View className="w-full flex-row justify-center mt-20 items-center">
+<Image source={icons.logo} className="w-12 h-10" />
+  </View>
+  <View className="my-5">
+    <Searchbutton placeholder="Search movies..."
+    value={searchQuery}
+    onChangeText={(text:string)=>setSearchQuery(text)}
+    />
+  </View>
+  {
+    loading&&(
+      <ActivityIndicator size="large" color="#0000f" className="my-3"/>
+    )
+  }
+  {
+    error&&(
+      <Text className="text-red-500 px-5 my-3">
+        Error:{error}
+      </Text>
+    )
+  }
+  {!loading && !error && searchQuery.trim() && movies?.length>0 &&(
+      <Text className="text-xl text-white font-bold">
+        Search Results for{' '}
+        <Text className="text-accent">{searchQuery}</Text>
+      </Text>
+    )
+  }
+  </>
+}
+ListEmptyComponent={
+  !loading && !error? (
+    <View className="mt-10 px-5">
+<Text className="text-center text-gray-500">
+  {searchQuery.trim()?"No movies found":"Search for a movie"}
+</Text>
+    </View>
+  ):null
+}
+/>      
        </View>
   )
 }
--- components/searchbutton.tsx
+++ components/searchbutton.tsx
@@ -4,12 +4,14 @@ import { icons } from '@/constants/icons'
 interface Props{
     placeholder:string;
     onPress?:()=>void;
+    value:string;
+    onChangeText:(text:string)=>void;
 }
-const searchbutton = ({placeholder,onPress}:Props) => {
+const searchbutton = ({placeholder,onPress,value,onChangeText}:Props) => {
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
 <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8bff"/>
-<TextInput onPress={onPress} placeholder={placeholder} value="" onChangeText={()=>{}} placeholderTextColor="#ab8bff" className="flex-1 ml-2 text-white"/>
+<TextInput onPress={onPress} placeholder={placeholder} value={value} onChangeText={onChangeText} placeholderTextColor="#ab8bff" className="flex-1 ml-2 text-white"/>
     </View>
   )
 }
